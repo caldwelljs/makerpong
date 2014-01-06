0c3ad38028140ab69e32c936c2b4ecf5a72fbd7a
Eric Boggs <eric.boggs@ac4d.com>
2014-01-06 13:59:19 -0600

    added create and destroy actions based on rails cast

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,20 @@
 class GamesController < ApplicationController
+
   def create
+   @game = current_user.games.build(:opponent_id => params[:opponent_id])
+   if @game.save
+     flash[:notice] = "Game successfully created."
+     redirect_to root_url
+   else
+     flash[:error] = "Unable to create game."
+     redirect_to root_url
+   end
   end
 
   def destroy
+    @game = Game.find(params[:id])
+    @game.destory
+    flash[:notice] = "Consider this game destroyed."
+    redirect_to root_url
   end
 end
