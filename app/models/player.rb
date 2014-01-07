fd9843f8b03e844c61d0c652a78b123d7ea7da19
Eric Boggs <eric.boggs@ac4d.com>
2014-01-06 18:40:08 -0600

    add inverse relationships per rails cast

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,9 +2,13 @@ class Player < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
+         # :validatable
 
   has_many :games
   has_many :opponents, through: :games
+
+  has_many :inverse_games, :class_name => "Game", :foreign_key => "game_id"
+  has_many :inverse_opponents, through: :inverse_opponents, :source => :player
 
 end
