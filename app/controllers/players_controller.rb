52a761c63e4eabe4f8f17f1213e86804e77841f6
MakerSquare Student <larshmatt@yahoo.com>
2014-01-11 15:07:35 -0600

    redirect to profile page when adding pic

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -28,7 +28,7 @@ class PlayersController < ApplicationController
   def update
     @player = Player.find(params[:id])
     @player.update(player_params)
-    redirect_to root_path
+    redirect_to @player
   end
 
   def destroy
