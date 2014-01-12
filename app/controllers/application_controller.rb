d74f21cbb960df5a2d85ca4699692662a442f536
Rich Edwards <richedw@gmail.com>
2014-01-12 13:10:15 -0600

    Add redirect in the application controller to send the player to the dashboard on sign-in. Add  old man ping pong photo to assets.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
-
+ 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  def after_sign_in_path_for(player)
+    params[:target] || welcome_dashboard_path(player.id)
+  end
 
   protected
     def configure_devise_permitted_parameters
