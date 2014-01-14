f0c576555aba9568ceb5d05ded1bb0bba7a5f0f6
Rich Edwards <richedw@gmail.com>
2014-01-14 16:04:58 -0600

    Moved photo editing to the settings page, change nav name, remove edit link from profile page, uncomment validatable on player model, add before action on application controller.

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,7 @@ class Player < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
-         # :validatable
+         :validatable
 
   has_many :games
   has_many :opponents, through: :games
