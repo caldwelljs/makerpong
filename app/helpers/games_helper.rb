ef45bc7e5f27c71ae678aadd6bfa1c73c0fcab85
John Caldwell <caldwelljs@gmail.com>
2014-01-15 15:57:24 -0600

    add conditional to worst loss display on dashboard and initial card styling to dashboard

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,11 @@
 module GamesHelper
+  def worst_loss_display
+    if Game.worst_loss.first.point_margin > 0
+      "#{Game.worst_loss.first.opponent.name} by #{Game.worst_loss.first.point_margin} points"
+    elsif Game.worst_loss.first.point_margin < 0
+      "#{Game.worst_loss.first.player.name} by #{Game.worst_loss.first.point_margin} points"
+    else
+      "See the worst beating here"
+    end
+  end
 end
