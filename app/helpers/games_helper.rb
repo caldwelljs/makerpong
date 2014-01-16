f38547ee7666f38da751976c9761524dd567ee93
MakerSquare Student <larshmatt@yahoo.com>
2014-01-16 16:13:48 -0600

    add rank style

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,11 +1,11 @@
 module GamesHelper
   def worst_loss_display
-    if Game.worst_loss.first.point_margin > 0
+    if Game.all.empty?
+    	"Player 1" 
+    elsif	Game.worst_loss.first.point_margin > 0
       "#{Game.worst_loss.first.opponent.name} by #{Game.worst_loss.first.point_margin} points"
     elsif Game.worst_loss.first.point_margin < 0
       "#{Game.worst_loss.first.player.name} by #{Game.worst_loss.first.point_margin} points"
-    else
-      "See the worst beating here"
     end
   end
 end
