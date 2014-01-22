41a15df8f5538db31d5f53ecbf67eac497d0dfbe
MakerSquare Student <larshmatt@yahoo.com>
2014-01-22 16:21:55 -0600

    amazon s3 changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,4 +5,10 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AS3_SECRET_ACCESS_KEY']
   }
   config.fog_directory = ENV['AS3_BUCKET_NAME']
+
+
+  AWS::S3::Base.establish_connection!(
+    :access_key_id     => ENV['S3_KEY'],
+    :secret_access_key => ENV['S3_SECRET']
+  )
 end
