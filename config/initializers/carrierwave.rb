b1243d17784eb3b3be8929ae42bbc8dc18ade367
MakerSquare Student <larshmatt@yahoo.com>
2014-01-22 20:08:42 -0600

    more trying heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => 'AKIAIN7JH7LKO4I5BUHA',       # required
     :aws_secret_access_key  => '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',       # required
-    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'makerpong-1' # required
   # see https://github.com/jnicklas/carrierwave#using-amazon-s3
