b2e8fab89f883bed7206a9b4f7067b9b2e27469a
John Caldwell <caldwelljs@gmail.com>
2014-01-24 12:37:29 -0600

    add worst loss pic and link to game/show on dashboard

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,8 @@
 module GamesHelper
+  
   def worst_loss_display
     if Game.all.empty?
-    	"Player 1" 
+    	"No Games Played" 
     elsif	Game.worst_loss.first.point_margin > 0
       "#{Game.worst_loss.first.opponent.name} by #{Game.worst_loss.first.point_margin}"
     elsif Game.worst_loss.first.point_margin < 0
