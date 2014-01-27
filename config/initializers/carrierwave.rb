642bb38a7f9589df97a801cc8725409f868a56b6
John Caldwell <caldwelljs@gmail.com>
2014-01-27 15:29:59 -0600

    merge error fix, return to previous commit if this throws error

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => 'AKIAIN7JH7LKO4I5BUHA',       # required
+    :aws_secret_access_key  => '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',       # required
+  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'makerpong-1' # required
+  # see https://github.com/jnicklas/carrierwave#using-amazon-s3
+  # for more optional configuration
+end
